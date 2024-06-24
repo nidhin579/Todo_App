84fb05f4495bac9a1a6dbb7882c28c9fdfde280f
nidhin579 <nidhinvarghese579@gmail.com>
2024-06-24 18:43:22 +0530

    Add basic firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ import 'package:todo_app/app/app.locator.dart';
 import 'package:todo_app/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:todo_app/ui/common/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await setupLocator();
   setupDialogUi();
   setupBottomSheetUi();
