6b1c3ce7b62aacf8577bf3a4cdc888e74d3866f9
nidhin579 <nidhinvarghese579@gmail.com>
2024-06-25 19:19:26 +0530

    Add loading state

--- lib/ui/views/home/widgets/task_list.dart
+++ lib/ui/views/home/widgets/task_list.dart
@@ -1,6 +1,7 @@
 import 'package:rive/rive.dart';
 import 'package:stacked/stacked.dart';
 import 'package:todo_app/entities/task_entity.dart';
+import 'package:todo_app/ui/common/ui_helpers.dart';
 import 'package:todo_app/ui/views/home/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/ui/views/home/widgets/widgets.dart';
@@ -13,14 +14,18 @@ class TaskList extends ViewModelWidget<HomeViewModel> {
     return StreamBuilder(
       stream: viewModel.taskReference.snapshots(),
       builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
         if ((snapshot.data?.docs.length ?? 0) < 1) {
           return Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(
-                  height: 260,
-                  child: RiveAnimation.asset(
-                      'assets/animations/sleepingball.riv')),
+              SizedBox(
+                height: screenHeightFraction(context, dividedBy: 4),
+                child: const RiveAnimation.asset(
+                    'assets/animations/sleepingball.riv'),
+              ),
               Text(
                 'No tasks here!',
                 style: Theme.of(context)
